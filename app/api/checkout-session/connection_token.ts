4dc1092875383c8639814709890b1a6e7958cbb4
Tim Sumner <timothy@sumner.eu.org>
2025-05-01 13:33:37 +0100

    Re-deploy, testing connection_token

--- app/api/checkout-session/connection_token.ts
+++ app/api/checkout-session/connection_token.ts
@@ -1,11 +1,9 @@
-// pages/api/connection_token.ts (Next.js or Vercel edge function)
+import { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-03-31.basil',
 })
-
-import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const token = await stripe.terminal.connectionTokens.create()
